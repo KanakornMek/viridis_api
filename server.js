1ddf5e6047602aeb3c1e2d6aedeb92468f105496
finmook <apimooksarovath@gmail.com>
2023-07-21 21:12:59 +0700

    new userApi from nif

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-require('dotenv').config();
-const verifyToken = require('./src/utils/verifyjwt')
-const tokenRouter = require('./src/routers/token')
-
+require("dotenv").config();
+const verifyToken = require("./src/utils/verifyjwt");
+const tokenRouter = require("./src/routers/token");
+const userRouter = require("./src/routers/user");
 
 app.use(express.json());
 
 const port = 4000;
-mongoose.connect(process.env.MONGO_URL)
-mongoose.connection.on('connected', () => {
-    console.log('connected to database')
-})
-app.use('/', verifyToken);
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on("connected", () => {
+  console.log("connected to database");
+});
+app.use("/", verifyToken);
 
- 
-
-app.use('/token', tokenRouter);
-
-
+app.use("/token", tokenRouter);
+app.use("/user", userRouter);
 
 app.listen(port, () => {
-    console.log(`listening to port ${port}`)
-})
+  console.log(`listening to port ${port}`);
+});
