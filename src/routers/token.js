9d0189b8cb3ad80f0526a71c04782a33284ff98d
Ttouch-thareewet <ttouchthareewet@gmail.com>
2023-07-21 10:26:40 +0700

    fix my bug
    fix my bug

--- src/routers/token.js
+++ src/routers/token.js
@@ -5,18 +5,22 @@ const User = require('../models/User');
 const Token = require('../models/Token');
 const Transactions = require('../models/Transactions')
 
-
+router.get('/', (req, res) => {
+    res.send(req.user.userId)
+})
 
 router.get('/purchase', async (req, res) => {
-    const amtToken = req.body.amountToken
+    if(!req.body.amtToken) return res.status(400).send()
+    const amtToken = parseInt(req.body.amtToken)
     const tokenPrice = await Token.findOne()
-    const totalPrice = amountToken * price
+    const totalPrice = amtToken * tokenPrice.price
     const userpurchsae = new Transactions
        ( {purchaseDate : Date.now(),
         tokenPrice,
         amtToken,
         totalPrice,
         point : totalPrice,
+        userId: req.user.userId,
         paymentId : Date.now()})
 
     userpurchsae.save();
